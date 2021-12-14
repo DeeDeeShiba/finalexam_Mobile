445b9ec55220776aa65abebf3ea970cff90567be
David Romero <a.davidromero@icloud.com>
2021-12-14 13:25:14 -0500

    Added Domain with Envelope and PetType

---
+++ app/src/main/java/sheridan/romeroad/finalexam/domain/Envelope.kt
@@ -0,0 +1,14 @@
+package sheridan.romeroad.finalexam.domain
+
+/**
+ * Student ID: 991555778
+ * finalexam
+ * created by davidromero
+ * on 2021-12-14
+ **/
+data class Envelope (
+        val name: String?,
+        val petType: PetType,
+        val age: Int,
+        val isVax: Boolean,
+        )
---
+++ app/src/main/java/sheridan/romeroad/finalexam/domain/PetType.kt
@@ -0,0 +1,9 @@
+package sheridan.romeroad.finalexam.domain
+
+/**
+ * Student ID: 991555778
+ * finalexam
+ * created by davidromero
+ * on 2021-12-14
+ **/
+enum class PetType {CAT, DOG, OTHER}
